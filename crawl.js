438c510db77aecea977baca414d7f79b53e2c4b3
Thibaut Broggi <broggi_t@epitech.eu>
2015-08-08 03:32:20 +0200

    adding comments for the quote

--- crawl.js
+++ crawl.js
@@ -30,20 +30,33 @@ function getLatestQuoteNumber(cb) {
 function getQuote(nb, cb) {
     getPage({host: 'danstonchat.com', path: '/' + nb + '.html', port: 80, method: 'GET'}, function(page) {
 	var $ = cheerio.load(page);
-	var quote = {content: [],
+	var quote = {id: nb,
+		     content: [],
 		     votes: {
-			 plus: $('#' + nb + ' > a.voteplus').text().split(' ')[1],
-			 minus: $('#' + nb + ' > a.voteminus').text().split(' ')[1],
-		     }
+			 plus: parseInt($('#' + nb + ' > a.voteplus').text().split(' ')[1]),
+			 minus: parseInt($('#' + nb + ' > a.voteminus').text().split(' ')[1]),
+		     },
+		     comments: []
 		    };
 	_.each($('#content > .item-entry > .item' + nb + ' > .item-content > a').html().split('<br>'), function(line) {
 	    quote.content.push({login: $('.decoration', line).text(),
 				line: $('<p>' + line.split('</span>')[1] + '</p>').text()});
 	});
-	console.log(quote);
+	$('#comments > .comment').each(function(i, item) {
+	    var comment = {author: {}};
+	    comment.content = $('<p>' + $('.comment-content > p', item).html().replace(/<br>/g, '\n') + '</p>').text();
+	    comment.plus = parseInt($('a.voteplus', item).text().split(' ')[1]);
+	    comment.minus = parseInt($('a.voteminus', item).text().split(' ')[1]);
+	    comment.author.name = $('.comment-content > a.gravatar > img', item).attr('alt');
+	    comment.author.id = parseInt($('.comment-content > a.gravatar').attr('href').split('.html')[0].split('/geek/')[1]);
+	    quote.comments.push(comment);
+	});
+	cb(quote);
     });
 }
 
 getLatestQuoteNumber(function(nb) {
-    getQuote(nb);
+    getQuote(nb, function(quote) {
+	console.log(quote);
+    });
 });
