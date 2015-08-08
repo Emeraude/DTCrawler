0adfadb83068624a93cc48a2fa626415d5834294
Thibaut Broggi <broggi_t@epitech.eu>
2015-08-08 03:13:33 +0200

    getting quote content

--- crawl.js
+++ crawl.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 var cheerio = require('cheerio');
+var _ = require('lodash');
 
 function getPage(options, cb) {
     var page = '';
@@ -15,9 +16,9 @@ function getPage(options, cb) {
 }
 
 function getLatestQuoteNumber(cb) {
-    var latestQuote;
     getPage({host: 'danstonchat.com', path: '/latest.html', port: 80, method: 'GET'}, function(page) {
 	var $ = cheerio.load(page);
+	var latestQuote;
 	$('#content > .item-listing > div.item').each(function(i, item) {
 	    if (!i)
 		latestQuote = parseInt($(item).attr('class').split(' ')[1].substr(4));
@@ -26,6 +27,18 @@ function getLatestQuoteNumber(cb) {
     });
 }
 
+function getQuote(nb, cb) {
+    getPage({host: 'danstonchat.com', path: '/' + nb + '.html', port: 80, method: 'GET'}, function(page) {
+	var $ = cheerio.load(page);
+	var quote = {content: []};
+	_.each($('#content > .item-entry > .item' + nb + ' > .item-content > a').html().split('<br>'), function(line) {
+	    quote.content.push({login: $('.decoration', line).text(),
+				line: $('<p>' + line.split('</span>')[1] + '</p>').text()});
+	});
+	console.log(quote);
+    });
+}
+
 getLatestQuoteNumber(function(nb) {
-    console.log(nb);
+    getQuote(nb);
 });
